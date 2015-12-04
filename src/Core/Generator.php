83fbc57a90bf52b6984dc9662c51bd3808648c8f
Kevin ALBERCA <kevin.alberca@supinternet.fr>
2015-12-04 11:47:15 +0100

    Generation  du fichier de configuration

--- src/Core/Generator.php
+++ src/Core/Generator.php
@@ -1,15 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: AwH
- * Date: 04/12/15
- * Time: 11:43
- */
 
 namespace Core;
 
-
 class Generator
 {
+    public static function createConfigFile($host, $dbname, $dbuser, $dbpass)
+    {
+        $config = [
+            "database_host" => $host,
+            "database_name" => $dbname,
+            "database_user" => $dbuser,
+            "database_password" => $dbpass,
+        ];
 
+        if(file_put_contents(__DIR__."/../../config.json", json_encode($config)))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
 }
--- suporm.php
+++ suporm.php
@@ -3,6 +3,10 @@
 require_once("vendor/autoload.php");
 
 
+# Declaration des objets qui vont nous servir
+$generator = new Core\Generator();
+
+
 if(!isset($argv[1]) && empty($argv[1]))
 {
     $argv[1] = "default";
@@ -11,6 +15,25 @@ if(!isset($argv[1]) && empty($argv[1]))
 # Un switch pour gerer les differents cas d'argument
 switch($argv[1])
 {
+
+# Generer un ficher de configuration
+    case "--config":
+        if(!empty($argv[2]) && isset($argv[2], $argv[3], $argv[4], $argv[5]))
+        {
+            if($generator::createConfigFile($argv[2], $argv[3], $argv[4], $argv[5]))
+            {
+                echo "Le fichier de configuration a bien été créé, il est disponible ici :\n".__DIR__."/config.json";
+            }
+            else
+            {
+                echo "Le fichier de configuration n'a pas pu être créer";
+            }
+        }
+        else
+        {
+            echo "Pour generer un fichier de configuration, merci d'utiliser la commande suivante :\nphp suporm --config DATABASE_HOST DATABASE_NAME DATABASE_USER DATABASE_PASSWORD";
+        }
+        break;
 # Le cas default pour gerer les options non reconnues
     default:
         echo "Liste des actions";
