22000209707d353eab24fee945a6460517feff0f
Kevin ALBERCA <kevin.alberca@supinternet.fr>
2015-12-15 14:20:33 +0100

    Mise a jour de la Demo

--- Demo/users.php
+++ Demo/users.php
@@ -9,25 +9,44 @@ require_once __DIR__."/../vendor/autoload.php";
 $ORM = new ORM(); // Declaration de l'ORM
 $user = new \Entity\Users(); // On instancie la classe Utilisateur
 
+
+$clauses =  [
+    "WHERE" => "id = 3",
+    "AND" => "name = 'Baptou'"
+];
+
+#################################################
+#            SELECTIONNE DES DONNEES            #
+#################################################
+
+// $ORM->select($user, $clauses);
+
 $user->setName("Baptou") // On lui mets un Nom
     ->setEmail("medrup@loscil.fr") // Email
     ->setPassword(sha1("DEBILE")) // Password
     ->setInscription_date(date("Y-m-d H:i:s")) // Inscription
     ->setLast_connexion(date("Y-m-d H:i:s")); // Derniere Connexion
 
-//if($ORM->save($user)){
-//    echo "DATA INSEREE"; //Si il n'y a aucun probleme on retourne ce message
-//    echo "<ul>
-//<li>".$user->getName()."</li>
-//<li>".$user->getEmail()."</li>
-//<li>".$user->getPassword()."</li>
-//<li>".$user->getInscription_date()."</li>
-//<li>".$user->getLast_connexion()."</li></ul>";
-//
-//} else {
-//    echo "ERROR"; // Sinon, on retourne une erreur
-//}
+############################################
+#            INSERT DES DONNEES            #
+############################################
 
-var_dump($ORM->delete($user, [
-    "WHERE" => "id = 4"
-]));
+ $ORM->save($user);
+
+#################################################
+#            MISE A JOUR DES DONNEES            #
+#################################################
+
+$user->setName("Baptiste") // On lui mets un Nom
+    ->setEmail("test@orm.dev") // Email
+    ->setPassword(sha1("Sup_ORM")) // Password
+    ->setInscription_date(date("Y-m-d H:i:s")) // Inscription
+    ->setLast_connexion(date("Y-m-d H:i:s")); // Derniere Connexion
+
+$ORM->update($user, $clauses);
+
+##############################################
+#            SUPPRIME DES DONNEES            #
+##############################################
+
+ $ORM->delete($user, $clauses);
