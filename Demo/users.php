bf7d96212b0cdb462453cc15a54edb2dbb1b105e
Kevin ALBERCA <kevin.alberca@supinternet.fr>
2015-12-15 22:10:28 +0100

    Edition de la Demo

--- Demo/users.php
+++ Demo/users.php
@@ -6,47 +6,48 @@
  * Time: 19:01
  */
 require_once __DIR__."/../vendor/autoload.php";
+
 $ORM = new ORM(); // Declaration de l'ORM
 $user = new \Entity\Users(); // On instancie la classe Utilisateur
 
-
 $clauses =  [
-    "WHERE" => "id = 3",
-    "AND" => "name = 'Baptou'"
+    "WHERE" => "name = 'Baptou'"
 ];
+
 
 #################################################
 #            SELECTIONNE DES DONNEES            #
 #################################################
-
 // $ORM->select($user, $clauses);
-
 $user->setName("Baptou") // On lui mets un Nom
-    ->setEmail("medrup@loscil.fr") // Email
-    ->setPassword(sha1("DEBILE")) // Password
-    ->setInscription_date(date("Y-m-d H:i:s")) // Inscription
-    ->setLast_connexion(date("Y-m-d H:i:s")); // Derniere Connexion
+->setEmail("medrup@loscil.fr") // Email
+->setPassword(sha1("DEBILE")) // Password
+->setInscription_date(date("Y-m-d H:i:s")); // Inscription
+
 
 ############################################
 #            INSERT DES DONNEES            #
 ############################################
-
- $ORM->save($user);
-
+/*
+if($ORM->save($user)){
+  echo "OK";
+} else {
+  echo "FAILED";
+}
+*/
 #################################################
 #            MISE A JOUR DES DONNEES            #
 #################################################
 
 $user->setName("Baptiste") // On lui mets un Nom
-    ->setEmail("test@orm.dev") // Email
-    ->setPassword(sha1("Sup_ORM")) // Password
-    ->setInscription_date(date("Y-m-d H:i:s")) // Inscription
-    ->setLast_connexion(date("Y-m-d H:i:s")); // Derniere Connexion
+->setEmail("test@orm.dev") // Email
+->setPassword(sha1("Sup_ORM")) // Password
+->setInscription_date(date("Y-m-d H:i:s")); // Inscription
 
 $ORM->update($user, $clauses);
+
 
 ##############################################
 #            SUPPRIME DES DONNEES            #
 ##############################################
-
- $ORM->delete($user, $clauses);
+$ORM->delete($user, $clauses);
